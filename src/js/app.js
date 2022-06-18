020608f9b3770fa8f0c29430bbf143ea6ec93585
Luis Escobedo <luis.escobedo@uinenlinea.mx>
2022-06-18 15:29:31 -0500

    se modifo el js para que se vean las imagenes

--- src/js/app.js
+++ src/js/app.js
@@ -10,8 +10,8 @@ function crearGaleria(){
     for(let i = 1; i <= 12; i++){
         const imagen=document.createElement("picture");
         imagen.innerHTML = `
-        <source srcset="../build/img/thumb/${i}.webp" type="image/webp" alt="La familia"/>
-        <img loading="lazy" width="200px" height="300px" src="../build/img/thumb/${i}.jpg" alt="imagen galeria"/>
+        <source srcset="../img/thumb/${i}.webp" type="image/webp" alt="La familia"/>
+        <img loading="lazy" width="200px" height="300px" src="../img/thumb/${i}.jpg" alt="imagen galeria"/>
           `; 
           
           galeria.appendChild(imagen);
